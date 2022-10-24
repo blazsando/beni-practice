85fc8eb3375f22b8dc91402f89c1a09fea8bda9c
andobalazsandras <balazs.ando@codecool.com>
2022-10-24 10:46:22 +0200

    add skeleton

---
+++ README.md
@@ -0,0 +1,18 @@
+Task 1:
+When HP reaches 0 for a Codemon button has no effect and winner is displayed in the div with "winner" class
+
+Task 2:
+Pass turn between codemons
+
+Task3:
+Type has effect on dmg as follows:
+fire-fire: normal damage
+water-water: normal damage
+wind-wind: normal damage
+water-fire: double damage
+fire-wind: double damage
+wind-water: double damage
+water-wind: half damage
+fire-water: half damage
+wind-fire: half damage
+
---
+++ scipt.js
@@ -0,0 +1,15 @@
+const codemon = 1;
+
+function attack(){
+    const attackInput = document.querySelector(`#attack-${codemon}`);
+    const attackStr = attackInput.value;
+    const attack = parseInt(attackStr);
+    const hpSpan = document.querySelector(`#hp-${(codemon+1) % 3}`);
+    const hpStr = hpSpan.innerHTML;
+    const hp = parseInt(hpStr);
+    hpSpan.innerHTML = Number(hp - attack).toString();
+
+}
+
+
+document.querySelector('button').addEventListener('click', attack)
