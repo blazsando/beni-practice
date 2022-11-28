b48209c398748639060a36db1726902a516d8c36
andobalazsandras <balazs.ando@codecool.com>
2022-11-28 09:41:53 +0100

    add bugfix for display

--- scipt.js
+++ scipt.js
@@ -4,7 +4,7 @@ function attack(){
     const attackInput = document.querySelector(`#attack-${codemon}`);
     const attackStr = attackInput.value;
     const attack = parseInt(attackStr);
-    const hpSpan = document.querySelector(`#hp-${(codemon+1) % 3}`);
+    const hpSpan = document.querySelector(`#hp-${(codemon+1) % 2}+1`);
     const hpStr = hpSpan.innerHTML;
     const hp = parseInt(hpStr);
     hpSpan.innerHTML = Number(hp - attack).toString();
